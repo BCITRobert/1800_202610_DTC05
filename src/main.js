24c2b5ab227144c2f7b4e54cc7bcc4f931b71465
Edward Kim <hkim503@my.bcit.ca>
2026-03-01 23:11:52 -0800

    Committed for updates

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
-// Importing Bootstrap and custom CSS in JavaScript take advantage of Vite’s build and development features.
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import {onAuthReady} from "./authentication.js"
 
-// If you have custom global styles, import them as well:
-import '/styles/style.css';
+function showName() {
+      const nameElement = document.getElementById("name-goes-here"); // the <h1> element to display "Hello, {name}"
 
-function sayHello() {
-  // TODO: implement your logic here
+      onAuthReady((user) => {
+          if (!user) {
+              // If no user is signed in → redirect back to login page.
+              location.href = "index.html";
+              return;
+          }
+
+          // If a user is logged in:
+          // Use their display name if available, otherwise show their email.
+          const name = user.displayName || user.email;
+
+          // Update the welcome message with their name/email.
+          if (nameElement) {
+              nameElement.textContent = `${name}!`;
+          }
+      });
 }
-document.addEventListener('DOMContentLoaded', sayHello);
+
+showName();
