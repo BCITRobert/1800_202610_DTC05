8c171e82ec994135a5d8d651ee72811f47cfdbe9
BCITRobert <rsi@my.bcit.ca>
2026-03-24 11:52:34 -0700

    commit

--- src/components/bottom_component.js
+++ src/components/bottom_component.js
@@ -9,6 +9,7 @@ class BottomBar extends HTMLElement {
                 <div class="flex items-center justify-center flex-wrap gap-2 w-full">
                     <form id="authControls" class="flex items-center gap-2 justify-evenly w-full">
                         <img class="w-20 px-4 py-1.5 border-2 border-gray-500 text-gray-600 font-bold rounded hover:bg-gray-500 hover:text-white transition h-16" src="./images/home-icon.svg" id="mainIcon">
+                        <img class="w-20 px-4 py-1.5 border-2 border-gray-500 text-gray-600 font-bold rounded hover:bg-gray-500 hover:text-white transition h-16" src="./images/create_route.svg" id="route">
                         <img class="w-20 px-4 py-1.5 border-2 border-gray-500 text-gray-600 font-bold rounded hover:bg-gray-500 hover:text-white transition h-16" src="./images/profile.svg" id="profile">
                     </form>
                 </div>
@@ -21,14 +22,18 @@ class BottomBar extends HTMLElement {
     updateAuth() {
         const profileBtn = this.querySelector("#profile")
         const mainBtn = this.querySelector("#mainIcon")
+        const routeBtn = this.querySelector("#route")
         // if (!authControls) return;
+
         onAuthStateChanged(auth, user => {
             if (user) {
-                mainBtn?.addEventListener('click', () => { window.location.href = 'main.html'})
+                mainBtn?.addEventListener('click', () => { window.location.href = 'main.html' })
                 profileBtn?.addEventListener('click', () => { window.location.href = `profile.html?userID=${user.uid}`; })
             } else {
+                mainBtn?.addEventListener('click', () => { window.location.href = 'index.html' })
                 profileBtn?.addEventListener('click', () => { window.location.href = 'login.html'; });
             }
+            routeBtn?.addEventListener('click', () => { window.location.href = `route.html`; })
         });
     }
 
