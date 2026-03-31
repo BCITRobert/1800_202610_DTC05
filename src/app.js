d8d1b12f041835b0673313bf677c1bacbb1011bc
BCITRobert <rsi@my.bcit.ca>
2026-03-31 08:57:12 -0700

    t

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 // We are using Tailwind for styling, so we don't need to import Bootstrap.
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap';
+import '/src/styles/style.css';
